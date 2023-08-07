8a444e0d3e1282ae3a47e4f6c935db7fd769056f
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-08-07 18:58:48 +0300

    Add files via upload

---
+++ 02-Fundamentals/02-Data-Types-and-Variables/solutions.js
@@ -0,0 +1,13 @@
+// // // Lab
+
+// // 01. Echo Type
+// function echoType(parameter) {
+//     let allowedTypes = ['string', 'number'];
+//     console.log(typeof parameter);
+//     if (allowedTypes.includes(typeof parameter) ) {
+//         console.log(parameter);
+//     }
+//     else {
+//         console.log('Parameter is not suitable for printing')
+//     }
+// }
