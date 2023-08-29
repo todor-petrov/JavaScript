3cc870aef85612c692f34ca7dec1c57f9cebf791
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-08-29 06:17:58 +0300

    Add files via upload

--- 04-DOM-and-Events/solutions.js
+++ 04-DOM-and-Events/solutions.js
@@ -354,3 +354,18 @@ function extract(content) {
     let matches = text.match(/\(([^()]*)\)/g).map(function($0) { return $0.substring(1,$0.length-1) });
     return matches.join('; ');
 }
+
+// 03. Mouse Gradient
+function attachGradientEvents() {
+    let gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', gradientMove);
+    gradient.addEventListener('mouseout', gradientOut);
+    function gradientMove(event) {
+        let power = event.offsetX / (event.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+        document.getElementById('result').textContent = power + "%";
+    }
+    function gradientOut(event) {
+        document.getElementById('result').textContent = "";
+    }
+}
